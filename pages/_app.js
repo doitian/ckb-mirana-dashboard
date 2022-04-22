0b996f1723b035ec3b58efecd35ae8b5300b7ce0
ian <ian@cryptape.com>
2022-04-22 09:12:47 +0800

    add title in _app.js
    https://nextjs.org/docs/messages/no-document-title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>CKB Mirana Dashboard</title>
+        <meta
+          name="description"
+          content="CKB Mainnet Hardfork Mirana Countdown and Monitoring."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>CKB Mirana Dashboard</title>
-        <meta
-          name="description"
-          content="CKB Mainnet Hardfork Mirana Countdown and Monitoring."
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
